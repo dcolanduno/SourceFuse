1af87a44d1b04c106df29bd346dc3bb35d2584df
Derek Colanduno <243282+dcolanduno@users.noreply.github.com>
2024-10-10 16:55:40 -0400

    Create provisionECSclusterloadbalancer.tf

---
+++ provisionECSclusterloadbalancer.tf
@@ -0,0 +1,22 @@
+resource "aws_ecs_cluster" "main" {
+  name = "my-ecs-cluster"
+}
+
+resource "aws_lb" "main" {
+  name               = "my-load-balancer"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = [aws_subnet.public.id]
+}
+
+resource "aws_security_group" "lb_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}

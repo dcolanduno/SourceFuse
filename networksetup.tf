79c563f7daaede1544f622a158b8633aabe533be
Derek Colanduno <243282+dcolanduno@users.noreply.github.com>
2024-10-10 16:54:21 -0400

    Create networksetup.tf

---
+++ networksetup.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "us-west-2" # Change to your desired region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-west-2a" # Change as needed
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "us-west-2a" # Change as needed
+}

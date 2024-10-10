5178d6ceea84d939062e053398d258bbfae6929c
Derek Colanduno <243282+dcolanduno@users.noreply.github.com>
2024-10-10 16:57:55 -0400

    Create targetlistenerforbalancer.tf

---
+++ targetlistenerforbalancer.tf
@@ -0,0 +1,23 @@
+resource "aws_lb_target_group" "nginx_target_group" {
+  name     = "nginx-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold  = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nginx_target_group.arn
+  }
+} 
